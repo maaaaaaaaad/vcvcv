589d6b3ca0923313b1f813e0b0d8d35cf893bb1f
maaaaaaaaad <integral5137@gmail.com>
2025-08-15 16:24:29 +0900

    remove all counter feature-related files and implementations

--- lib/data/datasources/counter_local_data_source.dart
+++
@@ -1,7 +0,0 @@
-class CounterLocalDataSource {
-  int _value = 0;
-
-  int getCounter() => _value;
-
-  int increment() => ++_value;
-}
--- lib/domain/entities/counter.dart
+++
@@ -1,5 +0,0 @@
-class Counter {
-  final int value;
-
-  const Counter(this.value);
-}
