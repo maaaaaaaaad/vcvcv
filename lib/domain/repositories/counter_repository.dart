55944841180e4b7e37939eb8c53d098c972cd284
maaaaaaaaad <integral5137@gmail.com>
2025-08-15 16:23:56 +0900

    remove counter feature, including repository, use cases, and related implementations

--- lib/domain/repositories/counter_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/counter.dart';
-import '../../core/usecase/usecase.dart';
-
-abstract class CounterRepository {
-  Future<Result<Counter>> getCounter();
-
-  Future<Result<Counter>> increment();
-}
