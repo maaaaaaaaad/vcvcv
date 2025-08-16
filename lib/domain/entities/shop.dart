31cb63469636252022caf95a1e079269cb8dff6d
maaaaaaaaad <integral5137@gmail.com>
2025-08-16 20:45:46 +0900

    add `id` and `thumbnailUrl` fields to `Shop` entity to support unique identification and image thumbnail usage

--- lib/domain/entities/shop.dart
+++ lib/domain/entities/shop.dart
@@ -1,13 +1,17 @@
 class Shop {
+  final String id;
   final String name;
   final String category;
   final double rating;
   final double distanceKm;
+  final String thumbnailUrl;
 
   const Shop({
+    required this.id,
     required this.name,
     required this.category,
     required this.rating,
     required this.distanceKm,
+    required this.thumbnailUrl,
   });
 }
